5c8eaddbb63a2cc39d475acd6482f437641d5eb4
Bruno Borges <brunofreitas.ads@gmail.com>
2024-04-21 20:12:28 -0300

    deploy

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 import fetch from "node-fetch";
 import cheerio from "cheerio";
-import fs from 'fs';
+import express from 'express';
 
-fetch('https://ceee.equatorialenergia.com.br/app/dp/DesligProgramado.xhtml')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then(html => {
-    const $ = cheerio.load(html);
+const app = express();
 
-    const dataList = [];
+app.get('/download', (req, res) => {
+    fetch('https://ceee.equatorialenergia.com.br/app/dp/DesligProgramado.xhtml')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            const $ = cheerio.load(html);
 
-    $('ul#listaDesligamentos_list > li.ui-datalist-item').each((index, element) => {
+            const dataList = [];
 
-      const title = $('.titulo', element).text().trim();
-      const hour = $('.hora', element).text().trim();
+            $('ul#listaDesligamentos_list > li.ui-datalist-item').each((index, element) => {
 
-      const ruasArray = $('ul#listaDesligamentos\\:0\\:j_idt34_list > li.itens-listas')
-      .map((index, element) => $(element).text().trim())
-      .get();
+                const title = $('.titulo', element).text().split("-");
+                const date = title[0].trim();
+                const city = title[2].trim();
+                const hour = $('.hora', element).text().trim();
 
-      const data = {
-        Titulo: title,
-        Hora: hour,
-        Ruas: ruasArray
-      };
+                const ruasArray = $('ul#listaDesligamentos\\:0\\:j_idt34_list > li.itens-listas')
+                    .map((index, element) => $(element).text().trim())
+                    .get();
 
-      dataList.push(data);
-    });
+                const data = {
+                    Data: date,
+                    Cidade: city,
+                    Hora: hour,
+                    Ruas: ruasArray
+                };
 
-     const jsonData = JSON.stringify(dataList, null, 2);
+                dataList.push(data);
+            });
 
-     fs.writeFile('data.json', jsonData, (err) => {
-       if (err) {
-         throw err;
-       }
-       console.log('Data has been written to data.json');
-     });
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
-  });
+            const jsonData = JSON.stringify(dataList, null, 2);
+       
+            res.set('Content-Disposition', 'attachment; filename="data.json"');
+            res.set('Content-Type', 'application/json');
+            res.send(jsonData);
+
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
